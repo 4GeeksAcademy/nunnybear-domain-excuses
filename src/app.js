68d90e610067240e71e0cd6f413337721bd53131
Jaylyn Stample <jaystample@gmail.com>
2023-06-12 15:33:45 -0700

    added ext generators
    to view the code use Live Server and open with the HTML file highlighted.

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ let domainName = () => {
   let article = ['the', 'three', 'last', 'even' ];
   let adj = ['wonderful', 'cool', 'grand', 'great', 'amazing'];
   let noun = ['gamers', 'women', 'cats', 'knights', 'mages'];
+  let ext = ['.org', '.com', '.net', '.gov']
 
   let articleIndex = Math.floor(Math.random() * article.length);
   let adjIndex = Math.floor(Math.random() * adj.length);
   let nounIndex = Math.floor(Math.random() * noun.length);
+  let extIndex = Math.floor(Math.random() * ext.length);
 
-  return article[articleIndex] + adj[adjIndex] + noun[nounIndex] + '.com';
+  return article[articleIndex] + adj[adjIndex] + noun[nounIndex] + ext[extIndex];
 
 };
