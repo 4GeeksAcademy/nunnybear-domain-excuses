3f97465b72047c81fb15c0a51ec71422e18f3556
Jaylyn Stample <jaystample@gmail.com>
2023-07-05 12:55:58 -0700

    fixed it finally!

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
+window.onload = () => {
+  document.querySelector("#website").innerHTML = domainName();
+};
+
 let domainName = () => {
-  let article = ['the', 'three', 'last', 'even' ];
-  let adj = ['wonderful', 'cool', 'grand', 'great', 'amazing'];
-  let noun = ['gamers', 'women', 'cats', 'knights', 'mages'];
-  let ext = ['.org', '.com', '.net', '.gov']
+  let article = ["the", "three", "last", "even"];
+  let adj = ["wonderful", "cool", "grand", "great", "amazing"];
+  let noun = ["gamers", "women", "cats", "knights", "mages"];
+  let ext = [".org", ".com", ".net", ".gov"];
 
   let articleIndex = Math.floor(Math.random() * article.length);
   let adjIndex = Math.floor(Math.random() * adj.length);
   let nounIndex = Math.floor(Math.random() * noun.length);
   let extIndex = Math.floor(Math.random() * ext.length);
 
-  return article[articleIndex] + adj[adjIndex] + noun[nounIndex] + ext[extIndex];
-
+  return (
+    article[articleIndex] + adj[adjIndex] + noun[nounIndex] + ext[extIndex]
+  );
 };
